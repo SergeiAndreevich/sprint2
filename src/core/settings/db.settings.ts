ff3e24738f0ac0d5303d843a18607e85341c230c
SergeiAndreevich <sergeytroshin00@gmail.com>
2025-06-22 20:47:16 +0300

    changed bd from local to remote

--- src/core/settings/db.settings.ts
+++ src/core/settings/db.settings.ts
@@ -1,10 +1,10 @@
-// export const SETTINGS = {
-//     PORT: process.env.PORT || 5005,
-//     MONGO_URL: process.env.MONGO_URL || 'mongodb+srv://sergeytroshin00:admin@test.f11is3f.mongodb.net/backend-project4?retryWrites=true&w=majority&appName=Test',
-//     DB_NAME: process.env.DB_NAME || 'backend-project4'
-// }
 export const SETTINGS = {
     PORT: process.env.PORT || 5005,
-    MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017',
-    DB_NAME: process.env.DB_NAME || 'blogsdb'
+    MONGO_URL: process.env.MONGO_URL || 'mongodb+srv://sergeytroshin00:admin@test.f11is3f.mongodb.net/backend-project4?retryWrites=true&w=majority&appName=Test',
+    DB_NAME: process.env.DB_NAME || 'backend-project4'
 }
+// export const SETTINGS = {
+//     PORT: process.env.PORT || 5005,
+//     MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017',
+//     DB_NAME: process.env.DB_NAME || 'blogsdb'
+// }
